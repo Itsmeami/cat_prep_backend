f8d1ebcc04fa83e19e099b219666e25ee1676c73
Aman Dubey <113337608+Itsmeami@users.noreply.github.com>
2026-04-22 16:32:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://cat-prep-frontend-9ktb.vercel.app'],
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
